01b98e3b4674e371816f73c317c9f3fa71941f5f
chendong <chendong0444@gmail.com>
2018-03-08 15:35:57 +0800

    debug wechat-pay payment method

--- tendenci/apps/payments/views.py
+++ tendenci/apps/payments/views.py
@@ -43,8 +43,8 @@ def pay_online(request, invoice_id, guid="", template_name="payments/pay_online.
         EventLog.objects.log(instance=invoice)
 
     # check payment exist
-    payments = Payment.objects.filter(Q(guid=guid))
-    if payments and payments.count() == 1 and payments[0].guid == guid:
+    payments = Payment.objects.filter(Q(invoice_id=invoice_id))
+    if payments and payments.count() == 1 and payments[0].invoice_id == invoice_id:
         template_name="payments/thankyou.html"
         payment = payments[0]
         return render_to_response(template_name, {'payment': payment}, context_instance=RequestContext(request))
