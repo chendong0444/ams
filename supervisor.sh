cb596b905d2f119f1dba90da7684c521a430c8bf
chendong <chendong0444@gmail.com>
2018-03-13 13:51:34 +0800

    edit 'tendencidemo' to 'demo'

--- supervisor.sh
+++ supervisor.sh
@@ -5,3 +5,7 @@ command = python /home/ubuntu/ams/manage.py runserver
 user = ubuntu
 autostart = true
 autorestart = true' >> /etc/supervisord.conf
+
+
+python manage.py dumpdata --indent 2 --exclude=files.MultipleFile --exclude=events.StandardRegForm --exclude=help_files> ams.json
+python manage.py dumpdata --indent 2 --exclude=files.MultipleFile --exclude=events.StandardRegForm --exclude=help_files --natural-foreign --natural-primary > ams.json
