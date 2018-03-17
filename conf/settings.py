9fdeccab868a154ad72e404b81070698c84c6737
chendong <chendong0444@gmail.com>
2018-03-17 16:34:01 +0800

    debug qiniu cloud storage

--- conf/settings.py
+++ conf/settings.py
@@ -110,7 +110,7 @@ if USE_S3_STORAGE:
     QINIU_SECRET_KEY = os.environ['QINIU_SECRET_KEY']
     QINIU_BUCKET_DOMAIN = 'cdn.ams365.cn'   # 'cdn.ams365.cn'   # 'idv1li2.qiniudns.com'  # 'p5mlwp2oe.bkt.clouddn.com'
     QINIU_BUCKET_NAME = 'ams365'
-    QINIU_SECURE_URL = True
+    QINIU_SECURE_URL = True     # 是否通过 HTTPS 来访问七牛云存储上的资源(若为'是', 可填True, true 或 1；若为'否', 可填False, false 或 0) 默认为否。
 
     DEFAULT_FILE_STORAGE = 'qiniustorage.backends.QiniuMediaStorage'
     STATICFILES_STORAGE = 'qiniustorage.backends.QiniuStaticStorage'
